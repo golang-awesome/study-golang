2eb1bdf2fcabb966459cf30f74e216375750f07a
Zhenglai Zhang <zhenglai.zhang@bkjk.com>
2019-05-27 21:27:52 +0800

    error handling

--- 003-select.go
+++ 003-select.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"runtime"
+	"sync"
 	"time"
 )
 
 func main() {
-	testGOMAXPROCS()
+	testRandStream()
 }
 
 func testGOMAXPROCS() {
@@ -107,4 +109,105 @@ loop:
 	}
 
 	fmt.Printf("Achieved %v cycles of work before signalled to Stop", workCounter)
+}
+
+func testForRange() {
+	stringStream := make(chan string, 3)
+	done := make(chan int)
+	var wg sync.WaitGroup
+	wg.Add(3)
+	for _, s := range []string{"a", "b", "c",} {
+		select {
+		case <-done:
+			return
+		case stringStream <- s:
+			wg.Done()
+		}
+	}
+
+	wg.Wait()
+	close(stringStream)
+	for v := range stringStream {
+		fmt.Printf("%v\n", v)
+	}
+}
+
+func testForSelectLoop() {
+	for { // Either loop infinitely or range over something
+		select {
+		// do some work with channels
+		}
+	}
+}
+
+func testDoneSignal() {
+	doWork := func(
+		done <-chan interface{},
+		strings <-chan string,
+	) <-chan interface{} {
+		terminated := make(chan interface{})
+		go func() {
+			defer fmt.Println("doWork exited")
+			defer close(terminated)
+			for {
+				select {
+				case s := <-strings:
+					fmt.Println("strings...")
+					fmt.Println(s)
+				case <-done:
+					fmt.Println("done signaled...")
+					return
+				}
+			}
+		}()
+		return terminated
+	}
+
+	done := make(chan interface{})
+	terminated := doWork(done, nil)
+	go func() {
+		// cancel the op after 1 second
+		time.Sleep(1 * time.Second)
+		fmt.Println("Canceling doWork goroutine...")
+		close(done)
+	}()
+
+	<-terminated
+	fmt.Println("Done.")
+}
+
+func testRandStream() {
+	newRandStream := func(done <-chan interface{}) <-chan int {
+		randStream := make(chan int)
+		go func() {
+			defer fmt.Println("newRandomStream closure exited")
+			defer close(randStream)
+			for {
+				select {
+				case randStream <- rand.Int():
+					fmt.Println("generating one int")
+				case <-done:
+					fmt.Println("singal done")
+					return
+				}
+			}
+		}()
+		return randStream
+	}
+
+	done := make(chan interface{})
+	randStream := newRandStream(done)
+	fmt.Println("3 random ints")
+	for i := 1; i < 4; i++ {
+		fmt.Printf("%d: %d\n", i, <-randStream)
+	}
+	close(done)
+
+	// simulate ongoing work
+	time.Sleep(1 * time.Second)
+	// 3 random ints
+	// 1: 5577006791947779410
+	// 2: 8674665223082153551
+	// 3: 6129484611666145821
+	// newRandomStream closure exited
 }
--- 004_size.go
+++ 004_size.go
@@ -5,6 +5,9 @@ import (
 	"unsafe"
 )
 
+/*
+The empty struct and empty interface, though syntactically similar, are actually opposites. An empty struct holds no data; an empty interface can hold any type of value. If I see a map[MyType]struct{}, I know immediately that no values will be stored, only keys. If I see a map[MyType]interface{}, my first impression will be that it is a heterogenous collection of values. Even if I see code storing nil in it, I won't know for sure that some other piece of code doesn't store something else in it.
+*/
 func main() {
 	var s struct{}
 	fmt.Println(unsafe.Sizeof(s))
