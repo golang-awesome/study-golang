b31b465c1e0ce4fd1dfadb361810baf89cdd248c
Zhenglai Zhang <zhenglai.zhang@bkjk.com>
2019-05-29 16:23:16 +0800

    add gin demos

--- gin/002-hello.go
+++ gin/002-hello.go
@@ -3,17 +3,90 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/labstack/gommon/log"
+	"io"
 	"net/http"
+	"os"
 )
 
 var File = struct{}{}
 
 func main() {
-	g := gin.New()
+	g := gin.Default()
+	g.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello World")
+	})
 
-	middlewares := []gin.HandlerFunc{}
+	// route param
+	g.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(http.StatusOK, "Hello %s", name)
+	})
+	g.GET("/user/:name/*action", func(c *gin.Context) {
+		name := c.Param("name")
+		action := c.Param("action")
+		c.String(http.StatusOK, name+" is "+action)
+	})
 
-	g.Use(middlewares...)
+	// query string
+	g.GET("/user", func(c *gin.Context) {
+		fistname := c.DefaultQuery("firstname", "Guest")
+		lastname := c.Query("lastname")
+		c.String(http.StatusOK, "hello %s %s", fistname, lastname)
+	})
 
-	fmt.Println(http.ListenAndServe(":8080", g).Error())
+	// form
+	// application/x-www-form-urlencoded
+	g.POST("/form_post", func(c *gin.Context) {
+		message := c.PostForm("message")
+		nick := c.DefaultPostForm("nick", "anonymous")
+		c.JSON(http.StatusOK, gin.H{
+			"status": gin.H{
+				"status_code": http.StatusOK,
+				"status":      http.StatusText(http.StatusOK),
+			},
+			"message": message,
+			"nick":    nick,
+		})
+	})
+
+	// form & put
+	g.PUT("/put", func(c *gin.Context) {
+		id := c.Query("id")
+		page := c.DefaultQuery("page", "0")
+		name := c.PostForm("name")
+		message := c.PostForm("message")
+		fmt.Printf("id: %s; page: %s; name: %s; message: %s \n", id, page, name, message)
+		c.JSON(http.StatusOK, gin.H{
+			"satus_code": http.StatusOK,
+		})
+	})
+
+	// form
+	// multipart/form-data
+	// file uploading
+	g.POST("/upload", func(c *gin.Context) {
+		name := c.PostForm("name")
+		fmt.Println(name)
+		file, header, err := c.Request.FormFile("file")
+		if err != nil {
+			c.String(http.StatusBadRequest, "Bad request: %v", err)
+			return
+		}
+
+		filename := header.Filename
+		out, err := os.Create(filename)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer out.Close()
+		_, err = io.Copy(out, file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		c.String(http.StatusOK, "upload successfully")
+	})
+
+	// body
+	g.Run(":8080")
 }
